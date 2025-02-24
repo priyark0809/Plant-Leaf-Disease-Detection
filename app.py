54ec37db4a706bed9c2a8d8c89db9efb15d2c5ec
priyark0809 <150041007+priyark0809@users.noreply.github.com>
2025-02-24 18:58:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if not os.path.exists(model_path):
 
 model_path = "trained_plant_disease_model.keras"
 def model_prediction(test_image):
-    model = tf.keras.model.load_model(model_path)
+    model = tf.keras.models.load_model(model_path)
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
@@ -27,15 +27,6 @@ def model_prediction(test_image):
 st.sidebar.title("Plant Disease Detection System for Sustainable Agriculture")
 app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
 #app_mode = st.sidebar.selectbox("Select Page",["Home"," ","Disease Recognition"])
-
-# import Image from pillow to open images
-from PIL import Image
-img = Image.open("Diseases.png")
-
-# display image using streamlit
-# width is used to set the width of an image
-st.image(img)
-
 #Main Page
 if(app_mode=="HOME"):
     st.markdown("<h1 style='text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html=True)
@@ -45,12 +36,12 @@ elif(app_mode=="DISEASE RECOGNITION"):
     st.header("Plant Disease Detection System for Sustainable Agriculture")
     test_image = st.file_uploader("Choose an Image:")
     if(st.button("Show Image")):
-        st.image(test_image,width=4,use_container_width=True)
+        st.image(test_image,width=4,use_column_width=True)
     #Predict button
     if(st.button("Predict")):
         st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
         #Reading Labels
-        class_name = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+        class_name = ['Potato__Early_blight', 'Potato_Late_blight', 'Potato__healthy']
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
